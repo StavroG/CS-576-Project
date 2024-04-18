be9ace3f7c27a77ad9f85cbd004f41d8b68f312e
Kenbo0601 <eug.ken0601@gmail.com>
2024-04-18 10:19:17 -0700

    working on chacha20

--- src/main/java/data/chacha20/CHACHA20.java
+++ src/main/java/data/chacha20/CHACHA20.java
@@ -1,14 +1,23 @@
 package data.chacha20;
 
-import data.Cipher;
+//import data.Cipher;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.ChaCha20ParameterSpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class CHACHA20 implements Cipher
 {
     //TODO: Need to implement this cipher
+    private static final String ALGORITHM = "ChaCha20";
+
 
     @Override
     public String encrypt(String message) throws Exception
     {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
         return "";
     }
 
