ae0df4718dda76deb3685fabf8375736c6fd6442
John Pateros <jpateros5410@sdsu.edu>
2024-04-27 13:02:46 -0700

    fixed Camellia encryption and added provider as dependency to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.68")
 }
 
 tasks.test {
